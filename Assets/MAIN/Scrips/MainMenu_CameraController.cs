52a147fba96c0a1508896592d00df26e05d34c9e
MinhNghia217 <trantruongminhnghia12345@gmail.com>
2024-05-07 18:47:53 +0700

    update main menu

--- Assets/MAIN/Scrips/MainMenu_CameraController.cs
+++ Assets/MAIN/Scrips/MainMenu_CameraController.cs
@@ -2,22 +2,62 @@ using Cinemachine;
 using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class MainMenu_CameraController : MonoBehaviour
 {
-    [SerializeField] GameObject mainCamera;
-    [SerializeField] GameObject chooseZooCamera;
+    [SerializeField] CinemachineVirtualCamera currentCamera;
+    [SerializeField] RectTransform panelMainMenu;
+    [SerializeField] GameObject cutScenceTimeLine;
+    [SerializeField] float topPosY, middlePosY;
+    [SerializeField] float tweenDuration;
 
-    public void SetMainCam()
+    public Ease easeInType;
+    public Ease easeOutType;
+
+
+    private void Start()
     {
-        mainCamera.SetActive(true);
-        chooseZooCamera.SetActive(false);
+        currentCamera.Priority++;
+        cutScenceTimeLine.SetActive(false);
     }
 
-    public void SetChooseZooCam()
+    public void BackMainMenu(CinemachineVirtualCamera target)
     {
-        mainCamera.SetActive(false);
-        chooseZooCamera.SetActive(true);
+        currentCamera.Priority--;
+
+        currentCamera = target;
+
+        currentCamera.Priority++;
+        panelMainMenu.DOAnchorPosY(middlePosY, tweenDuration).SetEase(easeInType);
+
+    }
+
+    public async void ChangeChooseZooCamera(CinemachineVirtualCamera target)
+    {
+        await panelMainMenu.DOAnchorPosY(topPosY, tweenDuration).SetEase(easeOutType).AsyncWaitForCompletion();
+        cutScenceTimeLine.SetActive(true);
+        currentCamera.Priority--;
+
+        currentCamera = target;
+        StartCoroutine(FinishCut());
+    }
+
+    public void ChangeMinigameCamera(CinemachineVirtualCamera target)
+    {
+        panelMainMenu.DOAnchorPosY(topPosY, tweenDuration);
+
+        currentCamera.Priority--;
+
+        currentCamera = target;
+
+        currentCamera.Priority++;
+    }
+
+    IEnumerator FinishCut()
+    {
+        yield return new WaitForSeconds(6f);
+        currentCamera.Priority++;
     }
 }
