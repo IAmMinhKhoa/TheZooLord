3f383289ad1c8fa8e9f6e879acbfcc4d7b46a357
Minh Khoa <88275892+IAmMinhKhoa@users.noreply.github.com>
2024-06-04 20:31:17 +0700

    123

---
+++ Assets/MAIN/Scrips/Config/LoadContent.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+public class LoadContent : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+    public void cc()
+    {
+        StartCoroutine(DownloadScenes());   
+    }
+    private IEnumerator DownloadScenes()
+    {
+        AsyncOperationHandle downloadHandle = Addressables.DownloadDependenciesAsync("Scene", true);
+
+        while (!downloadHandle.IsDone)
+        {
+            float progress = downloadHandle.PercentComplete;
+
+            // progressBar.fillAmount = progress;
+            Debug.Log("loading:" + progress);
+
+            yield return null;
+        }
+
+        
+        // Get bundle version
+        string bundleVersion = Addressables.GetDownloadSizeAsync("Scene").Result.ToString();
+
+        
+        yield return downloadHandle.IsDone;
+        Common.LoadSceneAsync(GameScenes.MainMenu);
+    }
+
+}
